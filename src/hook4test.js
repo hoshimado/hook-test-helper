b01fa9dd6f354186f311fe1b0590c727a394107d
xingyanhuan <xingyanhuan@yahoo.co.jp>
2018-07-08 14:28:54 +0900

    コメントを追加（復元）。日本語の扱いどうする？
    npm用なので、日本語のコメントってありなのかなぁ？

--- src/hook4test.js
+++ src/hook4test.js
@@ -37,6 +37,13 @@ RESTORE_MANAGER.prototype.restore = function () {
 };
 
 
+/**
+ * 指定されたオブジェクトのプロパティを全て退避＆undefinedに設定した後に、
+ * 指定したstubのプロパティを接続し直す。
+ * 
+ * @param {*} targetObject     プロパティを差し替えるオブジェクト。
+ * @param {*} stubPropertyMap  差換え後のオブジェクト。これが接続される。
+ */
 var hookProperty = function ( targetObject, stubPropertyMap ) {
 	var originalMaps = {};
 	var stubed = targetObject;
@@ -44,12 +51,16 @@ var hookProperty = function ( targetObject, stubPropertyMap ) {
 	var keys = Object.keys( targetObject );
 	var n = keys.length;
 
+	// オリジナルのpropertyを退避する
 	while(0<n--){
 		originalMaps[ keys[n] ] = targetObject[ keys[n] ];
 
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+		// delete演算子も検討したが「意図しないプロパティが居る」ことが分かるように、indefined代入とした。
 		targetObject[ keys[n] ] = undefined;
 	}
 
+	// スタブとして渡されたpropertyへ差換える。
 	keys = Object.keys( stubPropertyMap );
 	n = keys.length;
 	while(0<n--){
@@ -59,6 +70,7 @@ var hookProperty = function ( targetObject, stubPropertyMap ) {
 	return new RESTORE_MANAGER( targetObject, originalMaps );
 };
 exports.hookProperty = hookProperty;
+
 
 
 
